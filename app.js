661367919106d3792fe049b038110427e331c210
suchiprageeth <suchiruprageeth94@gmail.com>
2024-06-26 21:39:06 +0530

    Implementing showing relevant section when related navigation button is clicked

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const sections = document.querySelectorAll(".section");
 const secBtns = document.querySelectorAll(".controls");
 const secBtn = document.querySelectorAll(".control");
-const allSection = document.querySelector(".main-content");
-console.log({ sections, secBtn, secBtns, allSection });
+const allSections = document.querySelector(".main-content");
+console.log({ sections, secBtn, secBtns, allSections });
 
 function PageTransitions() {
   // Button click active class
@@ -16,6 +16,28 @@ function PageTransitions() {
       this.className += " active-btn";
     });
   }
+
+  // Adding active class to sections when related navigation button is clicked
+  allSections.addEventListener("click", (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+      // Remove active class from all
+      secBtns.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      e.target.classList.add("active");
+
+      //Hide other sections
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+
+      const element = document.getElementById(id);
+      element.classList.add("active");
+    }
+  });
 }
 
 PageTransitions();
